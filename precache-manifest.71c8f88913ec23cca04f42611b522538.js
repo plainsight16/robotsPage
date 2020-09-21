b73d06e67de9a66d04de7358795df33d8409bc7c
plainsight16 <timmybaks2002@gmail.com>
2020-09-21 15:48:27 +0100

    lets try this again

---
+++ precache-manifest.71c8f88913ec23cca04f42611b522538.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c915626f41f20ed6571e8c54e29582",
+    "url": "/robotsPage/index.html"
+  },
+  {
+    "revision": "23c7717ed76223b8f3d8",
+    "url": "/robotsPage/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7f23d9ed71bc46f5e235",
+    "url": "/robotsPage/static/css/main.b05be19a.chunk.css"
+  },
+  {
+    "revision": "23c7717ed76223b8f3d8",
+    "url": "/robotsPage/static/js/2.9383d403.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robotsPage/static/js/2.9383d403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f23d9ed71bc46f5e235",
+    "url": "/robotsPage/static/js/main.32260ce5.chunk.js"
+  },
+  {
+    "revision": "0b804a04808f35044457",
+    "url": "/robotsPage/static/js/runtime-main.eb284485.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsPage/static/media/SEGA.12a3db8b.woff"
+  }
+]);
